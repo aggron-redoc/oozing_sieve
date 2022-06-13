db51cf60bdd19f98765b4ffa665b4d394fc270a1
aggron2002 <arun.physics500@gmail.com>
2022-06-13 22:26:23 +0530

    Kickoff

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const app = express();
+
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+app.post('/sieve',(req,res)=>{
+
+})
